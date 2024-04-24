feb9df9327aabb7437c748a5fca0c46da2778608
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-04-24 17:55:33 +0530

    fetched top 100 songs on tiktok

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,15 @@ import Heading from '@/components/Heading'
 import Quote from '@/components/Quote'
 import RandomButton from '@/components/RandomButton'
 
-const Index = ({data}) => {
+const Index = ({ data, songs }) => {
   const quotes = data.quotes
-  const [index,setIndex] = useState(0)
+  const [index, setIndex] = useState(0)
 
   const setQuoteRandom = () => {
     setIndex(Math.floor(Math.random() * 50))
   }
 
-  useEffect(setQuoteRandom,[])
+  useEffect(setQuoteRandom, [])
 
   return (
     <div className='flex flex-col h-screen'>
@@ -23,12 +23,26 @@ const Index = ({data}) => {
 }
 
 export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch(`https://make-me-feel-better.vercel.app/api/getQuotes`)
-  const data = await res.json()
+  const getData = async (link, headers) => {
+    const res = await fetch(link,
+      {
+        method:"GET",
+        headers:headers
+      }
+    )
+    const data = await res.json()
+    return data
+  }
+
+  // Fetch quotes
+  const quotes = await getData(`https://make-me-feel-better.vercel.app/api/getQuotes`, {})
+  const songs = await getData(`https://customer.api.soundcharts.com/api/v2/chart/tiktok/music/weekly/ranking/latest?offset=0&limit=100"`,{
+    'x-app-id': 'soundcharts',
+    'x-api-key': 'soundcharts'
+})
 
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data: quotes, songs } }
 }
 
 export default Index
