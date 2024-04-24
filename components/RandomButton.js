305a89b71e6d234f7bfe19ca6b8e0c96e238e77d
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-04-24 17:06:21 +0530

    finished generate random button

---
+++ components/RandomButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RandomButton = () => {
+  return (
+    <button className='mx-auto p-3 rounded-md my-auto bg-orange-300 hover:bg-orange-400'>
+      Generate New
+    </button>
+  )
+}
+
+export default RandomButton
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Heading from '@/components/Heading'
 import Quote from '@/components/Quote'
+import RandomButton from '@/components/RandomButton'
 
 const Index = () => {
   return (
     <div className='flex flex-col h-screen'>
       <Heading />
       <Quote text={"dummy text"} />
+      <RandomButton />
     </div>
   )
 }
