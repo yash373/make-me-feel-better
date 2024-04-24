0d3ea7654435ea0786e40ec1b450f3ec444c9031
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-04-24 17:26:49 +0530

    added ssr and useffect hook to gerate random quote on load

--- components/Quote.js
+++ components/Quote.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Quote = ({ text }) => {
     return (
         <div className='flex mx-auto my-auto rounded-md text-2xl h-[40vh] bg-orange-200 md:w-[50%]'>
-            <p className='mx-auto my-auto'>
+            <p className='mx-auto my-auto text-center w-[80%]'>
                 {text}
             </p>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Heading from '@/components/Heading'
 import Quote from '@/components/Quote'
 import RandomButton from '@/components/RandomButton'
 
-const Index = () => {
+const Index = ({data}) => {
+  const quotes = data.quotes
+  const [index,setIndex] = useState(0)
+
+  useEffect(()=>{
+    setIndex(Math.floor(Math.random() * 50))
+  },[])
+
   return (
     <div className='flex flex-col h-screen'>
       <Heading />
-      <Quote text={"dummy text"} />
+      <Quote text={quotes[index]} />
       <RandomButton />
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://make-me-feel-better.vercel.app/api/getQuotes`)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
 }
 
 export default Index
