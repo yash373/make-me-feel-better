04d11451fc32e4aa82ed7664ce6b6bd1ce62c6d2
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-04-24 17:03:51 +0530

    finished quote component

---
+++ components/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Quote = ({ text }) => {
+    return (
+        <div className='flex mx-auto my-auto rounded-md text-2xl h-[40vh] bg-orange-200 md:w-[50%]'>
+            <p className='mx-auto my-auto'>
+                {text}
+            </p>
+        </div>
+    )
+}
+
+export default Quote
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Heading from '@/components/Heading'
+import Quote from '@/components/Quote'
 
 const Index = () => {
   return (
     <div className='flex flex-col h-screen'>
       <Heading />
+      <Quote text={"dummy text"} />
     </div>
   )
 }
