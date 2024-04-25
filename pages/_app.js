9352b47e032493733716870acbf8f78aa69f27df
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-04-25 09:26:48 +0530

    Added a title

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Instant Motivational Quote Generator</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
